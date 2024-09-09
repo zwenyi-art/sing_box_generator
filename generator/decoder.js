3dc044586d9c9665f472b9c78063f332c761a469
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-09-09 21:12:30 +0630

    shadowsock method detector add

--- generator/decoder.js
+++ generator/decoder.js
@@ -34,6 +34,26 @@ const shadowSockLink = async (url) => {
   const hash = decodeURIComponent(hashPart);
   const userInfo = base64Decode(encodedUserInfo);
   const [method, password] = userInfo.split(":");
+  const methodList = [
+    "2022-blake3-aes-128-gcm",
+    "2022-blake3-aes-256-gcm",
+    "2022-blake3-chacha20-poly1305",
+    "none",
+    "aes-128-gcm",
+    "aes-192-gcm",
+    "aes-256-gcm",
+    "chacha20-ietf-poly1305",
+    "xchacha20-ietf-poly1305",
+    "aes-128-ctr",
+    "aes-192-ctr",
+    "aes-256-ctr",
+    "aes-128-cfb",
+    "aes-192-cfb",
+    "aes-256-cfb",
+    "rc4-md5",
+    "chacha20-ietf",
+    "xchacha20",
+  ];
   const result = {
     type: "shadowsocks",
     method: method,
@@ -45,7 +65,7 @@ const shadowSockLink = async (url) => {
   if (host === "127.0.0.1") {
     return;
   }
-  if (method === "ss") {
+  if (!methodList.includes(method)) {
     return;
   }
   return result;
