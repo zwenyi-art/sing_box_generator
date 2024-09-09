258663cf980df8d7ad2e8a7e9648481f8a996af5
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-09-09 20:38:23 +0630

    vercel config add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const sing_boxGen = require("../sing_box_generator/generator/sing_boxGen");
 const app = express();
+app.use(cors());
 app.get("/api/v1", sing_boxGen);
 app.listen(8080, () => {
   console.log("listening on port 8080");
